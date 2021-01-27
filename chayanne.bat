f55a3bf7a897e7395ad7350a797bf4c6c0e71fb6
laloinsane <calisto.michel@gmail.com>
2021-01-26 21:01:30 -0300

    update functions

--- chayanne.bat
+++ chayanne.bat
@@ -2,14 +2,13 @@
 
 title chayanne
 set batpath="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\chayanne.bat"
-
 set /P user=Nombre de Usuario:
 set /P theme=Nombre del Tema a Cargar:
 
 if exist %batpath% (
 	del /F /Q %batpath%
 )
-
+:change
 echo @echo off>%batpath%
 echo set rm="C:\Users\%user%\AppData\Local\Microsoft\Windows\Themes\Anime By Brasileno2010.theme">>%batpath%
 echo set th=%theme%>>%batpath%
@@ -19,11 +18,23 @@ echo )>>%batpath%
 echo if exist %th% (>>%batpath%
 echo rundll32.exe %SystemRoot%\system32\shell32.dll,Control_RunDLL %SystemRoot%\system32\desk.cpl desk,@Themes /Action:OpenTheme /file:"C:\Windows\Resources\Themes\%theme%.theme">>%batpath%
 echo )>>%batpath%
+echo Al iniciar la computadora, Se cargara el tema %theme% al usuario %user%.
+echo.
+echo.
+echo 1.-Reiniciar Computadora.
+echo 2.-Cambiar Parametros.
+echo 3.-Salir.
 
-echo Al iniciar la computadora, Se cargará el tema %theme% al usuario %user%.
-pause  
+set /P option=¿Que quieres hacer? :
 
-shutdown -r -f -t 5
+if %option% == 1 (
+	shutdown -r -f -t 5
+)
 
-exit
+if %option% == 2 (
+	goto change
+)
 
+if %option% == 3 (
+	exit
+)
