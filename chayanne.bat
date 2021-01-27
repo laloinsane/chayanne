461dbeb6eb9f9072b50df9ba3f5e7aeb07adf5e5
laloinsane <calisto.michel@gmail.com>
2021-01-26 21:11:45 -0300

    update variables

--- chayanne.bat
+++ chayanne.bat
@@ -1,31 +1,34 @@
 @echo off
 
 title chayanne
+:change
 set batpath="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\chayanne.bat"
+set rm="C:\Users\%user%\AppData\Local\Microsoft\Windows\Themes\Anime By Brasileno2010.theme">>%batpath%
+set th=%theme%>>%batpath%
+
 set /P user=Nombre de Usuario:
 set /P theme=Nombre del Tema a Cargar:
 
 if exist %batpath% (
 	del /F /Q %batpath%
 )
-:change
+
 echo @echo off>%batpath%
-echo set rm="C:\Users\%user%\AppData\Local\Microsoft\Windows\Themes\Anime By Brasileno2010.theme">>%batpath%
-echo set th=%theme%>>%batpath%
 echo if exist %rm% (>>%batpath%
 echo del /F /Q %rm%>>%batpath%
 echo )>>%batpath%
 echo if exist %th% (>>%batpath%
 echo rundll32.exe %SystemRoot%\system32\shell32.dll,Control_RunDLL %SystemRoot%\system32\desk.cpl desk,@Themes /Action:OpenTheme /file:"C:\Windows\Resources\Themes\%theme%.theme">>%batpath%
 echo )>>%batpath%
-echo Al iniciar la computadora, Se cargara el tema %theme% al usuario %user%.
+echo Al iniciar la computadora, Se cargara el tema "%theme%" al usuario "%user%".
 echo.
 echo.
 echo 1.-Reiniciar Computadora.
 echo 2.-Cambiar Parametros.
 echo 3.-Salir.
-
-set /P option=¿Que quieres hacer? :
+echo.
+echo.
+set /P option=Que quieres hacer? :
 
 if %option% == 1 (
 	shutdown -r -f -t 5
