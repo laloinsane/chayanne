57e45b50e6330ea00007aea55ae553cd1441587f
laloinsane <calisto.michel@gmail.com>
2021-01-28 11:50:17 -0300

    update hour

--- dose.bat
+++ dose.bat
@@ -15,7 +15,7 @@ for /F "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
     set "YYYY=%dt:~0,4%"
     set "MM=%dt:~4,2%"
     set "DD=%dt:~6,2%"
-    set "HH"=%dt:~8,2%"
+    set "HH=%dt:~8,2%"
     set "Min=%dt:~10,2%"
     set "Sec=%dt:~12,2%"
 
